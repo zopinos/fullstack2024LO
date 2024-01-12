e7a2f40d9b42265b7ddcf11dc1ec2a8243540575
zopinos <zopi.tv@gmail.com>
2024-01-12 17:30:52 +0200

    osa1 anekdootit

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -1,34 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+
+  const [selected, setSelected] = useState(0)
+
+  const randomPos = (until) => Math.floor(Math.random() * until)
+
+  const voteAnecdote = (idx) => {
+    const pointsCopy = [...points]
+    pointsCopy[idx] += 1
+    setPoints(pointsCopy)
+  }
+
+  const findBestIdx = () => points.indexOf(Math.max(...points))
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button text="vote" handleClick={() => { voteAnecdote(selected) }} />
+      <Button text="next anecdote" handleClick={() => { setSelected(randomPos(anecdotes.length)) }} />
+
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[findBestIdx()]}</p>
+      <p>has {points[findBestIdx()]} votes</p>
+    </div>
   )
 }
 
